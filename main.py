d7eec44c3889df7d790840725a1c29adbd7ca00a
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2024-10-13 20:57:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import os
 from flask import Flask
 
+app = Flask(__name__)
 CURRENT_PATH = os.path.dirname(__file__)
 MAP_PATH = f"{CURRENT_PATH}/coffee_map.html"
 
-app = Flask(__name__)
-
+@app.route("/")
 def start():
     with open(MAP_PATH, "r") as file:
         return file.read()
-
-app.add_url_rule("/", "main", start)
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
